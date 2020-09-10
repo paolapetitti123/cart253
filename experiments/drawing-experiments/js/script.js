e38fa709a4293d5aefbee00b2c1cc7d0075527bf
paolap.98 <paolapetitti123@gmail.com>
2020-09-10 15:50:49 -0400

    Drawing experiment: color added

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -11,7 +11,9 @@ Here is a description of this template p5 project.
 function setup()
 {
   createCanvas(500,500);
-  background(255,138,1);
+  background(191,255,199);
+  noStroke();
+
 /*
 Drawing Shapes Video:
   point(250,250);
@@ -21,10 +23,20 @@ Drawing Shapes Video:
   ellipse(250,250,100,100);
 */
 ellipseMode(CORNER);
+
+fill(127,0,200,100);
 ellipse(250,250,100,100);
+
+fill(137,0,210,100);
 ellipse(250,250,80,80);
+
+fill(147,0,220,100);
 ellipse(250,250,60,60);
+
+fill(157,0,230,100);
 ellipse(250,250,40,40);
+
+fill(167,0,240,100);
 ellipse(250,250,20,20);
 
 }
