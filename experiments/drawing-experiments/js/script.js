c5eaf45f2fc65fec8e5df7a5585d77508787745c
paolap.98 <paolapetitti123@gmail.com>
2020-09-10 15:23:24 -0400

    Drawing Experiment: drew a cone

--- experiments/drawing-experiments/js/script.js
+++ experiments/drawing-experiments/js/script.js
@@ -8,7 +8,24 @@ Here is a description of this template p5 project.
 // setup()
 //
 // Description of setup() goes here.
-function setup() {
+function setup()
+{
+  createCanvas(500,500);
+  background(255,138,1);
+/*
+Drawing Shapes Video:
+  point(250,250);
+  rect(0,0,250,250);
+  line(0,0,500,500);
+  line(500,0,0,500);
+  ellipse(250,250,100,100);
+*/
+ellipseMode(CORNER);
+ellipse(250,250,100,100);
+ellipse(250,250,80,80);
+ellipse(250,250,60,60);
+ellipse(250,250,40,40);
+ellipse(250,250,20,20);
 
 }
 
