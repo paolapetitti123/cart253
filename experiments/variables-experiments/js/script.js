b62854eb52cff6b5ae7f2a1164ccbbea50cca4fa
paolap.98 <paolapetitti123@gmail.com>
2020-09-15 16:40:51 -0400

    added messed to the console log

--- experiments/variables-experiments/js/script.js
+++ experiments/variables-experiments/js/script.js
@@ -30,4 +30,7 @@ function draw()
   background(backgroundShade);
   circle.x += circle.speed;
   ellipse(circle.x, circle.y, circle.size);
+
+  console.log(`circleX: ${circle.x}`);
+  console.log("circleY:" + circle.y);
 }
