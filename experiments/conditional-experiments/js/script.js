65926b48816e5d3a4e02a6ad5d2e52ac125951a1
paolap.98 <paolapetitti123@gmail.com>
2020-09-22 16:49:28 -0400

    Added if statements to make circle bounce from one side to the other repeatedly

--- experiments/conditional-experiments/js/script.js
+++ experiments/conditional-experiments/js/script.js
@@ -1,20 +1,49 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Conditional Experiments
+Paola Petitti
 
-Here is a description of this template p5 project.
+This project will be experimenting with conditional expressions
 **************************************************/
+// Variable declaration
+let bgShade = 0;
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  speed: 5
+}
+
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(500,500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(bgShade);
 
+  circle.x += circle.speed;
+
+  if (circle.x > width) {
+    circle.speed = -circle.speed;
+  }
+
+  if (circle.x < 0){
+    circle.speed = -circle.speed;
+  }
+
+  if (mouseY < height/2){
+    fill(255,0,0);
+  }
+
+  if (mouseY > height/2) {
+    fill(0,0,255);
+  }
+
+  ellipse(circle.x,circle.y,circle.size);
 }
