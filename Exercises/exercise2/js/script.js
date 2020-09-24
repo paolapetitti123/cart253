21c0e1a1b54e983d8e179901d588dda8457e5eb3
paolap.98 <paolapetitti123@gmail.com>
2020-09-24 13:46:20 -0400

    E2: Started exercise 2

---
+++ Exercises/exercise2/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Exercise 2: Dodge-em
+Paola Petitti
+
+WRITE DESCRPITION
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+  
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
