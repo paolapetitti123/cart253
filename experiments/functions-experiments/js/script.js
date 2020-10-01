e8fd5243b6d329ff44f99231429da8e910b5e2c1
paolap.98 <paolapetitti123@gmail.com>
2020-09-30 21:32:27 -0400

    changed functions to a parallels function that takes 6 parameters

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -5,13 +5,7 @@ Paola Petitti
 These are experiments with functions
 **************************************************/
 // Variable declarations
-let circle = {
-  x: 0,
-  y: 250,
-  size: 100,
-  vx: 1,
-  vy: 0
-}
+
 
 // setup()
 //
@@ -25,36 +19,18 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(0);
-  move();
-  wrap();
-  display();
+  parallels(100,100,5,1,100,1);
+  parallels(50,50,10,2,20,10);
+  parallels(200,200,15,7,3,20);
+  parallels(312,257,20,0.5,300,2);
 }
 
-function move(){
-  circle.x += circle.vx;
-  circle.y += circle.vy;
-}
-
-function wrap(){
-  if (circle.x > width){
-    reset();
-  }
-}
-
-function display(){
-    fill(255,0,0);
+function parallels(x,y,numLines,lineWidth,lineHeight,lineSpace){
+  for(let i = 0; i < numLines; i++){
+    noStroke();
+    fill(255);
     rectMode(CENTER);
-    ellipse(circle.x,circle.y,circle.size);
-}
-
-function reset(){
-  circle.x = 0;
-  circle.vx += 2;
-  circle.vy -= 0.25;
-  circle.size += 5;
-}
-
-function mousePressed()
-{
-  reset();
+    rect(x,y,lineWidth,lineHeight);
+    x += lineSpace;
+  }
 }
