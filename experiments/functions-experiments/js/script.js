feecbf2acb34f4bf1f8aec43d1eeda1b2d2c79c7
paolap.98 <paolapetitti123@gmail.com>
2020-09-30 21:05:30 -0400

    Added a function for move, wrap, display, reset and mousePressed

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -2,19 +2,59 @@
 Functions experiments
 Paola Petitti
 
-Here is a description of this template p5 project.
+These are experiments with functions
 **************************************************/
+// Variable declarations
+let circle = {
+  x: 0,
+  y: 250,
+  size: 100,
+  vx: 1,
+  vy: 0
+}
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(500,500);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
+  move();
+  wrap();
+  display();
+}
 
+function move(){
+  circle.x += circle.vx;
+  circle.y += circle.vy;
+}
+
+function wrap(){
+  if (circle.x > width){
+    reset();
+  }
+}
+
+function display(){
+    fill(255,0,0);
+    rectMode(CENTER);
+    ellipse(circle.x,circle.y,circle.size);
+}
+
+function reset(){
+  circle.x = 0;
+  circle.vx += 2;
+  circle.vy -= 0.25;
+  circle.size += 5;
+}
+
+function mousePressed()
+{
+  reset();
 }
