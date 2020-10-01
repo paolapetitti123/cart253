6e93a10f3e6440e2e91614af809d8541a6a3c039
paolap.98 <paolapetitti123@gmail.com>
2020-10-01 13:30:34 -0400

    A5: Starting activity 5

---
+++ Activities/05-looking-for-love/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Activity 05: Looking for love
+Paola Petitti
+
+WRITE DESCRIPTION HERE 
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
