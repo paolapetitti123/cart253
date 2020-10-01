f2560aac57b1537f4149ffe522d71058a8a3d67d
paolap.98 <paolapetitti123@gmail.com>
2020-10-01 11:58:18 -0400

    finished keyboard input lecture video

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -5,64 +5,51 @@ Paola Petitti
 These are experiments with functions
 **************************************************/
 let circle = {
-  x: 0,
+  x: 250,
   y: 250,
-  size: 100,
   vx: 0,
   vy: 0,
-  speed: 2,
-};
-
-let state = `title`; // Possible states are: title, animation, ending
+  speed: 5,
+  size: 100
+}
 
 function setup() {
   createCanvas(500, 500);
-  circle.vx = circle.speed;
-  textSize(32);
-  textAlign(CENTER,CENTER);
 }
 
 function draw() {
   background(0);
 
-  if(state === `title`){
-    // Title
-    title();
+  // First check for horizontal movement
+  // Is the left arrow pressed?
+  if (keyIsDown(LEFT_ARROW)) {
+    // If it is, set the x velocity to be negative
+    circle.vx = -circle.speed;
   }
-  else if(state === `animation`){
-    // Animation code
-    animation();
+  // Otherwise is the right arrow pressed?
+  else if (keyIsDown(RIGHT_ARROW)) {
+    // If it is, set the x velocity to be positive
+    circle.vx = circle.speed;
   }
-  else if(state === `ending`) {
-    // End
-    ending();
+  // If neither of those keys are pressed...
+  else {
+    // Then set the x velocity to 0 to stop moving horizontally
+    circle.vx = 0;
   }
-}
 
-function title(){
-  fill(255);
-  text(`Life.`,width/2,height/2);
-}
+  // Do the same thing with vertical movement and the UP and DOWN keys
+  if (keyIsDown(UP_ARROW)) {
+    circle.vy = -circle.speed;
+  }
+  else if (keyIsDown(DOWN_ARROW)) {
+    circle.vy = circle.speed;
+  }
+  else {
+    circle.vy = 0;
+  }
 
-function animation(){
   circle.x = circle.x + circle.vx;
   circle.y = circle.y + circle.vy;
 
-  if(circle.x > width){
-    state = `ending`;
-  }
-
   ellipse(circle.x, circle.y, circle.size);
-}
-
-function ending(){
-  fill(127);
-  text(`The End`, width/2, height/2);
-}
-
-function keyPressed(){
-  if(state === `title`){
-    state = `animation`;
-  }
-
 }
