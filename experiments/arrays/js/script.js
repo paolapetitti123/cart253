ccd3a43a944279fa8c394a33d1f2cedd83b9d87f
paolap.98 <paolapetitti123@gmail.com>
2020-10-19 13:46:56 -0400

    Started doing array experiments

---
+++ experiments/arrays/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Arrays Experiments
+Paola Petitti
+
+Following along with the lecture videos 
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
