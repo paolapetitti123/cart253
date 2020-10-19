c74f60ab02c0f0e6f5e75465375488fe5fd67975
paolap.98 <paolapetitti123@gmail.com>
2020-10-19 16:26:52 -0400

    Arrays exercises complete, ever refresh shows a new picture

--- experiments/arrays/js/script.js
+++ experiments/arrays/js/script.js
@@ -6,17 +6,22 @@ Following along with the lecture videos
 **************************************************/
 
 "use strict";
-let circle = {
-  x: 0,
-  y: 0,
-  size: 100,
-  trail: [],
-  trailSize: 20
+
+let images = [];
+let displayImage;
+
+function preload(){
+  for (let i = 0; i < 10; i++){
+    let sangyeonImages = loadImage(`assets/images/picture-${i}.png`);
+    images.push(sangyeonImages);
+  }
+
 }
 
 function setup() {
-  createCanvas(600,600);
+  createCanvas(windowWidth,windowHeight);
 
+  displayImage = random(images);
 }
 
 
@@ -25,23 +30,8 @@ function setup() {
 function draw() {
   background(0);
 
-  circle.x = mouseX;
-  circle.y = mouseY;
-
-  for (let i = 0; i < circle.trail.length; i++){
-    let position = circle.trail[i];
-    ellipse(position.x, position.y, circle.size);
-  }
-
-  ellipse(circle.x, circle.y, circle.size);
-
-  let newTrailPosition = {
-    x: circle.x,
-    y: circle.y
-  };
-  circle.trail.push(newTrailPosition);
-
-  if(circle.trail.length > circle.trailSize){
-    circle.trail.shift();
-  }
+  push();
+  imageMode(CENTER);
+  image(displayImage,width/2,height/2);
+  pop();
 }
