ea1a7384f4bc8ebf0f531fb06ed57ca32c33622b
paolap.98 <paolapetitti123@gmail.com>
2020-10-26 19:27:23 -0400

    A7: added Ball.js file

--- Activities/07-object-oriented-programming-activity/js/Paddle.js
+++ Activities/07-object-oriented-programming-activity/js/Paddle.js
@@ -5,4 +5,19 @@ class Paddle {
     this.x = 0;
     this.y = height - this.height/2;
   }
+
+  // This method allows the paddle to move
+  move() {
+    this.x = mouseX;
+  }
+
+  // This method allows the paddle to be seen
+  display() {
+    push();
+    fill(255);
+    noStroke();
+    rectMode(CENTER);
+    rect(this.x,this.y,this.width,this.height);
+    pop();
+  }  
 }
--- Activities/07-object-oriented-programming-activity/js/script.js
+++ Activities/07-object-oriented-programming-activity/js/script.js
@@ -7,16 +7,22 @@ Here is a description of this template p5 project.
 
 "use strict";
 
+let paddle;
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(windowWidth,windowHeight);
+  paddle = new Paddle(150,30);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
+  paddle.move();
+  paddle.display();
 }
