4dd3fb88a10ef24effc92bb8ca92a06a9a1c582a
paolap.98 <paolapetitti123@gmail.com>
2020-10-26 14:09:19 -0400

    Started OOP experiments!

---
+++ experiments/introOOP/js/script.js
@@ -0,0 +1,20 @@
+/**************************************************
+Intro to object oriented programming
+Paola Petitti
+
+Following along the videos!
+**************************************************/
+
+// setup()
+//
+// Description of setup() goes here.
+function setup() {
+
+}
+
+// draw()
+//
+// Description of draw() goes here.
+function draw() {
+
+}
