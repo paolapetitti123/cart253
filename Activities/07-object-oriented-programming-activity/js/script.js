e90f9b58d222e3af1ce4e278716eaa3854b16279
paolap.98 <paolapetitti123@gmail.com>
2020-10-26 20:02:44 -0400

    A7: added the constructor, gravity, move, bounce and display for the ball.js file (finished)

--- Activities/07-object-oriented-programming-activity/js/script.js
+++ Activities/07-object-oriented-programming-activity/js/script.js
@@ -2,12 +2,16 @@
 OOP Activity
 Paola Petitti
 
-Here is a description of this template p5 project.
+Juggling simulator
 **************************************************/
 
 "use strict";
+let gravityForce = 0.0025;
 
 let paddle;
+
+let balls = [];
+let numBalls = 3;
 
 // setup()
 //
@@ -15,6 +19,13 @@ let paddle;
 function setup() {
   createCanvas(windowWidth,windowHeight);
   paddle = new Paddle(150,30);
+
+  for (let i = 0; i < numBalls; i++){
+    let x = random(0, width);
+    let y = random(-400,-100);
+    let ball = new Ball(x,y);
+    balls.push(ball);
+  }
 }
 
 // draw()
@@ -25,4 +36,14 @@ function draw() {
 
   paddle.move();
   paddle.display();
+
+  for(let i = 0; i < balls.length; i++){
+    let ball = balls[i];
+    if(ball.active) {
+      ball.gravity(gravityForce);
+      ball.move();
+      ball.bounce(paddle);
+      ball.display();
+    }
+  }
 }
