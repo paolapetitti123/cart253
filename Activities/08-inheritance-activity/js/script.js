16c0d35f5174e6c321547924a389f6b954879b35
paolap.98 <paolapetitti123@gmail.com>
2020-11-03 15:43:53 -0500

    A8: added states, display text and different functions

--- Activities/08-inheritance-activity/js/script.js
+++ Activities/08-inheritance-activity/js/script.js
@@ -2,13 +2,57 @@
 Inheritance Activity
 Paola Petitti
 **************************************************/
-
+let state = `title`;
 
 function setup() {
-
+createCanvas(windowWidth, windowHeight);
 }
 
 
 function draw() {
+  background(0);
 
+  if(state === `title`){
+    title();
+  }
+  else if(state === `simulation`){
+    simulation();
+  }
+  else if(state === `success`){
+    success();
+  }
+  else if(state === `dead`){
+    dead();
+  }
+}
+
+function title(){
+  displayText(`PEDESTRIAN PALAVER!`);
+}
+
+function simulation(){
+
+}
+
+function success(){
+displayText(`YOU MADE IT!`);
+}
+
+function dead(){
+displayText(`YOU DIED!`);
+}
+
+function displayText(string){
+  push();
+  textAlign(CENTER,CENTER);
+  textSize(32);
+  fill(255);
+  text(string, width/2, height/2);
+  pop();
+}
+
+function keyPressed(){
+  if(state === `title`){
+    state = `simulation`;
+  }
 }
