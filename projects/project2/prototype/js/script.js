482e39af3c08895b4e720a5875a680a959b9ffa8
paolap.98 <paolapetitti123@gmail.com>
2020-11-02 20:16:59 -0500

    P2: started prototype, changed background, fixed Stealer.js code to be a class and added comments

---
+++ projects/project2/prototype/js/script.js
@@ -0,0 +1,429 @@
+/**************************************************
+Project 1: The Stealer
+Paola Petitti
+
+You must jump over the metal bars that are coming out of the floor
+to make it to the end of the level and steal the diamond heart. The player
+can jump onto the boxes however you can't jump when your on a box (I might try
+changing that if time permits).
+
+The idea/concept for this is heavily inspired by
+The Boyz - The Stealer music video
+This is the link if you are interested in watching it
+https://www.youtube.com/watch?v=c_e-IC0VwZM&ab_channel=THEBOYZ
+
+Hit sound effect: https://freesound.org/people/CastIronCarousel/sounds/216783/
+Gameplay music: https://freemusicarchive.org/search?adv=1&quicksearch=Monplaisir%20Garage&&
+Game win music: https://freemusicarchive.org/search?adv=1&quicksearch=Monplaisir%20Victory&&
+Game over sound: https://freesound.org/people/Euphrosyyn/sounds/442127/
+
+All the art (background, sprites, crates/boxes) was done by me
+**************************************************/
+/*
+  Variable declarations
+*/
+let bgImg;
+let bgLeft = 0;
+let moveSpeed = 15;
+let heartLeft = 0;
+let heartImg;
+let heartStolenImg;
+let heartD = {
+  x: 7000,
+  y: 448,
+  size: 350
+}
+let crateImg;
+let crate = {
+  x: undefined,
+  y: 547,
+  sizeX: 150,
+  sizeY: 150,
+  size: 150
+};
+let robberStandImg;
+let robberWalkImg;
+let bar = {
+  x: 0,
+  y: 622,
+  sizeX: 15,
+  sizeY: 0
+}
+let bottom = 0;
+let cieling = -496;
+let livesCounter = 3;
+let heartLivesImg;
+let introImg;
+let state = 'start';
+let gameMusic;
+let gameWin;
+let gameOver;
+let punchSound;
+let buttonEasy;
+let difficulty = 0;
+let diffImg;
+
+/*
+  Loading all the images and music!
+*/
+function preload(){
+  bgImg = loadImage('assets/images/stealerBackground.png');
+  heartImg = loadImage('assets/images/diamondHeart.png');
+  heartStolenImg = loadImage('assets/images/diamondHeartStolen.png');
+  crateImg = loadImage('assets/images/crate.png');
+  robberStandImg = loadImage('assets/images/stealer/robber-standing.gif');
+  robberWalkImg = loadImage('assets/images/stealer/robber-walking.gif');
+  heartLivesImg = loadImage('assets/images/stealer/heart.png');
+  transparentImg = loadImage('assets/images/stealer/transparent.png');
+  introImg = loadImage('assets/images/introScreen.png');
+  diffImg = loadImage('assets/images/selectDifficulty.png');
+
+  gameMusic = loadSound('assets/sounds/Monplaisir_-_02_-_Garage.mp3');
+  gameWin = loadSound('assets/sounds/Monplaisir_-_08_-_Victory.mp3');
+  gameOver = loadSound('assets/sounds/gameOver.wav');
+  punchSound = loadSound('assets/sounds/punch.mp3');
+
+}
+
+/*
+  Initializing stealer object, creating canvas and setting the
+  volume for the sounds.
+*/
+function setup() {
+  createCanvas(1280,720);
+  stealer = new Stealer(100, height + 50);
+  gameMusic.setVolume(0.5);
+  gameOver.setVolume(0.2);
+  gameWin.setVolume(0.3);
+}
+
+/*
+  Shows background, tells game which state to enter
+*/
+function draw() {
+  background(0);
+  backgroundMove();
+  crateShow();
+  if(state === `start`){
+    intro();
+  }
+  else if(state === `diffSelect`){
+    diffSelect();
+  }
+  else if(state === `simulation`){
+    simulation();
+  }
+  else if(state === `winEnding`){
+    winEnding();
+  }
+  else if(state === `loseEnding`){
+    loseEnding();
+  }
+}
+
+/*
+  This function calls all the functions needed to make the game run!
+*/
+function simulation() {
+  heartDiamond();
+  stealer.move();
+  handleKey();
+  barShow();
+  showLives();
+}
+
+/*
+  Functions to move the background
+*/
+function backgroundMove(){
+  imageMode(CORNER);
+  bgImg.resize(7250,720);
+  image(bgImg, bgLeft, 0);
+}
+function moveBgLeft(){
+  let minBgLeft = -bgImg.width + width;
+
+  if (bgLeft - moveSpeed > minBgLeft){
+    bgLeft -= moveSpeed;
+  }
+}
+function moveBgRight(){
+  if(bgLeft + moveSpeed < 0){
+    bgLeft += moveSpeed;
+  }
+}
+
+/*
+  Function to see which arrow keys are being held down to move
+  the background & character
+*/
+function handleKey(){
+  if(keyIsDown(LEFT_ARROW)){
+    if(stealer.canMoveLeft()){
+      stealer.moveLeft();
+      display(robberWalkImg, stealer.size2 + 10, stealer.size);
+    } else {
+      moveBgRight();
+      display(robberWalkImg, stealer.size2 + 10, stealer.size);
+    }
+  }
+
+  else if (keyIsDown(RIGHT_ARROW)){
+    if(stealer.canMoveRight()){
+      stealer.moveRight();
+      display(robberWalkImg, stealer.size2 + 10, stealer.size);
+    }
+    else {
+      moveBgLeft();
+      display(robberWalkImg, stealer.size2 + 10, stealer.size);
+      }
+    }
+    else {
+      display(robberStandImg, stealer.size2, stealer.size);
+    }
+}
+
+/*
+  Function to see if the UP arrow gets pressed once to jump
+*/
+function keyPressed(){
+  if (keyCode === UP_ARROW){
+    stealer.jump();
+  }
+  else if(keyCode === ENTER && state == `start`){
+    state = `diffSelect`;
+  }
+  else if(state == `diffSelect` && key == '1'){
+      difficulty = 1;
+      state = `simulation`;
+      gameMusic.play();
+    }
+  else if(state == `diffSelect` && key == '2'){
+      difficulty = 2;
+      state = `simulation`;
+      gameMusic.play();
+    }
+  else if(state == `diffSelect` && key == '3'){
+      difficulty = 3;
+      state = `simulation`;
+      gameMusic.play();
+    }
+  }
+
+/*
+  Function that changes the gif being displayed
+*/
+function display(picture, sizeX, sizeY){
+  imageMode(CENTER);
+  image(picture,stealer.pos.x, stealer.pos.y, sizeX, sizeY);
+}
+
+/*
+  Displaying the diamond at the end of the level that you need to reach in order
+  to win the level.
+*/
+function heartDiamond(){
+  let hrtX = bgLeft + 7000;
+  imageMode(CENTER);
+  image(heartImg, hrtX, heartD.y,heartD.size, heartD.size);
+  hrtTouch(hrtX);
+}
+function hrtTouch(heartX){
+  let d = dist(stealer.pos.x, stealer.pos.y, heartX, heartD.y);
+  if(d < stealer.size/2 + heartD.size/2){
+    state = `winEnding`;
+    gameMusic.stop();
+    gameWin.play();
+  }
+}
+
+/*
+  The following 4 crate functions show the crates, detect if you touch one & if
+  you are currently on a crate, if you are on a crate the bars can't hurt you.
+*/
+function crateShow(){
+  crate.x = bgLeft + 500;
+  imageMode(CENTER);
+    image(crateImg, crate.x, crate.y,crate.sizeX, crate.sizeY);
+    crateTouch(crate.x);
+  for(let j = 0; j < 5; j++){
+    crate.x  += 1000;
+
+      imageMode(CENTER);
+      image(crateImg, crate.x, crate.y,crate.sizeX, crate.sizeY);
+      crateTouch(crate.x);
+
+  }
+}
+function crateTouch(crateX){
+  if(isCrateTouching(crateX)){
+    stealer.vy = 0;
+    if(stealer.pos.y >= 520){
+      if(stealer.pos.x < crateX){
+        stealer.pos.x -= 15;
+      }
+      else{
+        stealer.pos.x += 15;
+      }
+    }
+    else if(stealer.pos.y < 520){
+      stealer.pos.x -= 0.01;
+    }
+   }
+
+
+}
+function isCrateTouching(crateX){
+  if(stealer.pos.x + stealer.r/2 > crateX - crate.sizeX/4 &&
+    stealer.pos.x - stealer.r/2 < crateX + crate.sizeX/4 &&
+    stealer.pos.y + stealer.r/2 >= crate.y - crate.sizeY/2  &&
+    stealer.pos.y - stealer.r/2 <= crate.y - crate.sizeY/2){
+      return true;
+    }
+ }
+
+/*
+  The 5 following functions show the bars, detect if you hit one and
+  what happens when you do hit one lose a life and get pushed back to try again
+  so long as you haven't lost all your lives.
+*/
+function barShow(){
+  let barX = bgLeft + 800;
+  fill(127);
+  noStroke();
+  rectMode(CORNER);
+
+  for(let i = 0; i < 9; i++){
+    rect(barX, bar.y, bar.sizeX, bar.sizeY);
+    barTouch(barX,bar.sizeY,bar.y);
+    barX += 600;
+
+  }
+  if(bar.sizeY <= bottom && bar.sizeY > cieling){
+    shouldGrow();
+  }
+
+  else if(bar.sizeY <= cieling && bar.sizeY < bottom){
+    shouldShrink();
+  }
+  else if(bar.sizeY == 0){
+    cieling = -496;
+  }
+}
+function shouldShrink(){
+  if(difficulty === 1){
+    cieling += 3;
+    bar.sizeY += 3;
+  }
+  else if(difficulty === 2){
+    cieling += 7;
+    bar.sizeY += 7;
+  }
+  else if(difficulty === 3){
+    cieling += 10;
+    bar.sizeY += 10;
+  }
+}
+function shouldGrow(){
+  if(difficulty === 1){
+    bar.sizeY -= 3;
+  }
+  else if(difficulty === 2){
+    bar.sizeY -= 7;
+  }
+  else if(difficulty === 3){
+    bar.sizeY -= 10;
+  }
+}
+function barTouch(barX, barSizeY, barY){
+  if (barIsTouching(barX, barSizeY, barY) && livesCounter >= 0)
+  {
+      if(stealer.pos.x >= barX){
+        stealer.jump();
+        stealer.pos.x += 100;
+        stealer.jump();
+        livesCounter -= 1;
+      }
+      else{
+        stealer.jump();
+        stealer.pos.x -= 100;
+        stealer.jump();
+        livesCounter -= 1;
+      }
+      fill(255,0,0,25);
+      rectMode(CORNER);
+      rect(0,0,1280,720);
+      punchSound.play();
+  }
+  else if (livesCounter < 0) {
+    state = `loseEnding`;
+    gameMusic.stop();
+    gameOver.play();
+  }
+
+
+}
+function barIsTouching(barX, barSizeY, barY){
+  if (stealer.pos.x + stealer.size/2 > barX - bar.sizeX/2 &&
+      stealer.pos.x - stealer.size/2 < barX + bar.sizeX/2 &&
+      stealer.pos.y + stealer.size/2 > barY + barSizeY &&
+      stealer.pos.y - stealer.size/2 < barY) {
+      return true;
+    }
+    else {
+      return false;
+    }
+}
+
+/*
+  This function is to display how many lives the player has left.
+*/
+function showLives(){
+  if(livesCounter == 3){
+    image(heartLivesImg, 50, 65, 25,25);
+    image(heartLivesImg, 80, 65, 25,25);
+    image(heartLivesImg, 110, 65, 25,25);
+  }
+  else if (livesCounter == 2){
+    image(heartLivesImg, 50, 65, 25,25);
+    image(heartLivesImg, 80, 65, 25,25);
+  }
+  else if(livesCounter == 1){
+    image(heartLivesImg, 50, 65, 25,25);
+  }
+  else if(livesCounter == 0) {
+    image(transparentImg, 50, 65, 25,25);
+  }
+}
+
+/*
+  The following 4 functions tell the program what to do for the start state,
+  the difficulty select state, the win state and the lose state.
+*/
+function intro(){
+  backgroundMove();
+  crateShow();
+  image(introImg, width/2, height/2);
+}
+function diffSelect(){
+  backgroundMove();
+  crateShow();
+  image(diffImg, width/2, height/2);
+}
+function winEnding(){
+  textSize(50);
+  textAlign(CENTER);
+  textFont("monospace");
+  fill(253, 139, 255);
+  let hrtX = bgLeft + 7000;
+  imageMode(CENTER);
+  image(heartStolenImg, hrtX, heartD.y,heartD.size, heartD.size);
+  text(`MISSION : SUCCESSFUL`, width/2, height/2);
+}
+function loseEnding(){
+  textSize(50);
+  textAlign(CENTER);
+  fill(0,0,0);
+  textFont("monospace");
+  text(`MISSION : FAILED`, width/2, height/2);
+}
---
+++ projects/project2/prototype/js/stealer.js
@@ -0,0 +1,71 @@
+class Stealer {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.r = 200;
+    this.vy = 0;
+    this.vx = 0;
+    this.gravity = 3;
+    this.speed = moveSpeed;
+    this.size = 200;
+    this.size2 = 85;
+    this.gravity = 3;
+    this.jumpHeight = -35;
+  }
+
+  // This function adds a force to simulate gravity
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  // This function is constantly updating the vel, acc & pos of the character
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  // This function allows for the character to move while being confined to the
+  // screen
+  move() {
+    this.pos.y += this.vy;
+    this.vy += this.gravity;
+    this.pos.y = constrain(this.pos.y, 0, height - this.r);
+  }
+
+  // This function allows for the character to jump
+  jump() {
+    if (this.pos.y === height - this.r) {
+      this.vy = this.jumpHeight;
+    }
+  }
+
+  // This function checks if it's possible to continue moving left
+  canMoveLeft() {
+    if (this.pos.x > 50 + this.speed) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // This function checks if it's possible to move to the right
+  canMoveRight() {
+    if (this.pos.x < width - (450 + this.speed)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // This function allows the character to move left
+  moveLeft() {
+    this.pos.x -= moveSpeed;
+  }
+
+  // This function allows the character to move right
+  moveRight() {
+    this.pos.x += moveSpeed;
+  }
+}
