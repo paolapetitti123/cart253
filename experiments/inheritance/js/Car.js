aaf16dfc178215fabed54d6b3b3dcf8c93665db4
paolap.98 <paolapetitti123@gmail.com>
2020-11-03 14:48:49 -0500

    Overrided move and wrap method and added veer method

--- experiments/inheritance/js/Car.js
+++ experiments/inheritance/js/Car.js
@@ -4,6 +4,29 @@ class Car extends Vehicle {
     this.width = 50;
     this.height = 20;
     this.vx = 5;
+    this.drunkness = 0.2;
+  }
+
+  move(){
+    super.move();
+    this.veer();
+  }
+
+  veer(){
+    let r = random();
+    if (r < this.drunkness){
+      this.vy = random(-5,5);
+    }
+  }
+
+  wrap(){
+    super.wrap();
+    if(this.y > height){
+      this.y -= height;
+    }
+    else if(this.y < 0){
+      this.y += height;
+    }
   }
 
   display() {
