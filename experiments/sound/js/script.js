94ed480e90bdfd397e7931105cf94fa3b3eb5dec
paolap.98 <paolapetitti123@gmail.com>
2020-11-09 14:39:12 -0500

    Changed the rate of the bark

--- experiments/sound/js/script.js
+++ experiments/sound/js/script.js
@@ -15,6 +15,7 @@ function preload(){
 // Description of setup() goes here.
 function setup() {
   createCanvas(600,600);
+  userStartAudio();
 }
 
 // draw()
@@ -22,6 +23,9 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(0);
+
+  let newRate = map(mouseX,0,width,-3,3);
+  barkSFX.rate(newRate);
 }
 
 function mousePressed(){
