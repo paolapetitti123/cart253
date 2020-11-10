9ac7b33ab06ac873a25dfec8d6bb255a2040c177
paolap.98 <paolapetitti123@gmail.com>
2020-11-09 20:25:51 -0500

    A9: Finished Activity 9 (followed along the video!)

--- Activities/09-sound-activity/js/script.js
+++ Activities/09-sound-activity/js/script.js
@@ -1,20 +1,45 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Activity 9: Sound Activity
+Paola Petitti
 
-Here is a description of this template p5 project.
+A program that plays music based on physics
 **************************************************/
+"use strict";
+
+// The balls
+let balls = [];
+
+// F-minor scale
+let notes = [`F3`,`G3`,`Ab4`,`Bb4`, `C4`, `Db4`, `Eb4`, `F4`];
 
 // setup()
 //
-// Description of setup() goes here.
+// Creating the canvas
 function setup() {
-
+  createCanvas(1280,720);
+  userStartAudio();
 }
 
 // draw()
 //
-// Description of draw() goes here.
+// Write comment here
 function draw() {
+  background(0);
 
+  for(let i = 0; i < balls.length; i++){
+    let ball = balls[i];
+    ball.move();
+    ball.bounce();
+    ball.display();
+  }
+}
+
+function mousePressed() {
+  createBall(mouseX,mouseY);
+}
+
+function createBall(x,y){
+  let note = random(notes);
+  let ball = new Ball(x,y,note);
+  balls.push(ball);
 }
