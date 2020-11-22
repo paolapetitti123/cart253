d8573b317e3873f215e4f29c48af0902288d898b
paolap.98 <paolapetitti123@gmail.com>
2020-11-22 14:57:04 -0500

    P2: starting level 3

---
+++ projects/project2/prototype/js/LevelThree.js
@@ -0,0 +1,6 @@
+class LevelThree extends LevelBuilder {
+  construction(bgLeft,bgImg,crateImg,heartLivesImg,transparentImg){
+    super(bgLeft,bgImg,crateImg,heartLivesImg,transparentImg);
+    this.bgLeft = bgLeft;
+  }
+}
