34486f92fd8680a099b5f8f77a4342cec057764e
paolap.98 <paolapetitti123@gmail.com>
2020-11-24 16:53:02 -0500

    P2: removed old code that wasn't working

--- projects/project2/prototype/js/Axe.js
+++ projects/project2/prototype/js/Axe.js
@@ -6,37 +6,19 @@ class Axe {
     this.h = 200;
     this.axeImg = axeImg;
     this.speed = 5;
-    this.rotation = 0;
   }
 
   move(){
     push();
-    // angleMode(DEGREES);
-    rotate(HALF_PI);
+
     pop();
   }
 
   display(x,y){
-    // let r = constrain(angle, -2, 2);
-    console.log(this.rotation);
-    angleMode(DEGREES);
     this.x = x;
     this.y = y;
-    push();
-    // if(this.rotation <= 2){
-    //   this.rotation -= 0.5;
-    //   if(this.rotation == -2 && this.rotation < 2){
-    //     this.rotation += 0.5;
-    //   }
-    // }
-    // translate(0,0);
-    // rotate(this.rotation);
     imageMode(CENTER);
     image(this.axeImg, x, y, this.w, this.h);
-
-
-    // this.rotation += 0.5;
-    pop();
   }
 
   axeTouch(){
